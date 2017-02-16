b5c1916f371382ee2b3eb4510472fc65306f916e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-16 16:03:30 +0000

    Create Evade and Pursue components based on Seek and Flee respectively

---
+++ Assets/scripts/Flee.cs
@@ -0,0 +1,13 @@
+﻿public class Flee : AgentBehaviour {
+
+	public override Steering GetSteering ()
+    {
+        Steering steering = new Steering ();
+        steering.linear = transform.position - target.transform.position;
+        steering.linear.Normalize ();
+        steering.linear *= agent.maxAccel;
+
+        return steering;
+    }
+
+}
---
+++ Assets/scripts/Seek.cs
@@ -0,0 +1,13 @@
+﻿public class Seek : AgentBehaviour {
+
+	public override Steering GetSteering ()
+    {
+        Steering steering = new Steering ();
+        steering.linear = target.transform.position - transform.position;
+        steering.linear.Normalize ();
+        steering.linear *= agent.maxAccel;
+
+        return steering;
+    }
+
+}
