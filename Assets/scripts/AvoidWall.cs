15595119c2fdcf9a634058d2208134e160740a24
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-16 21:30:46 +0000

    Add wallk avoidance behaviour

---
+++ Assets/scripts/AvoidWall.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class AvoidWall : Seek {
+
+	public float avoidDistance;
+    public float lookAhead;
+
+    public override void Awake ()
+    {
+        base.Awake ();
+        target = new GameObject ();
+    }
+
+    public override Steering GetSteering ()
+    {
+        Steering steering = new Steering ();
+        Vector3 position = transform.position;
+        Vector3 rayVector = agent.velocity.normalized * lookAhead;
+        Vector3 direction = rayVector;
+
+        RaycastHit hit;
+        if (Physics.Raycast (position, direction, out hit, lookAhead))
+        {
+            position = hit.point + hit.normal * avoidDistance;
+            target.transform.position = position;
+            steering = base.GetSteering ();
+        }
+
+        return steering;
+    }
+
+}
