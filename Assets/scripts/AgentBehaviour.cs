dadf2cf5b57fdfc0f6601280675455341a1a6498
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-16 16:45:18 +0000

    Create Face behaviour based on Align

--- Assets/scripts/AgentBehaviour.cs
+++ Assets/scripts/AgentBehaviour.cs
@@ -5,6 +5,11 @@ public class AgentBehaviour : MonoBehaviour {
     public GameObject target;
     protected Agent agent;
 
+    public float maxSpeed;
+    public float maxAccel;
+    public float maxRotation;
+    public float maxAngularAccel;
+
     public virtual void Awake ()
     {
         agent = GetComponent<Agent> ();
@@ -18,6 +23,23 @@ public class AgentBehaviour : MonoBehaviour {
     public virtual Steering GetSteering ()
     {
         return new Steering ();
+    }
+
+    public float MapToRange (float rotation)
+    {
+        rotation %= 360f;
+        if (Mathf.Abs (rotation) > 180f)
+        {
+            if (rotation < 0f)
+            {
+                rotation += 360f;
+            }
+            else
+            {
+                rotation -= 360f;
+            }
+        }
+        return rotation;
     }
 
 }
